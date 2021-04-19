6d24077e63ce7ec66158a92b5102f9cab9142705
chenzheng1995 <852110522@qq.com>
2021-04-19 09:33:12 +0800

    测试

--- src/test/abc/test123.java
+++ src/test/abc/test123.java
@@ -6,6 +6,7 @@ public class test {
 		// TODO Auto-generated method stub
       System.out.println("这是测试代码");
 		System.out.println("类名首字母要大写");
+		System.out.println("----------------------");
 	}
 
 }
