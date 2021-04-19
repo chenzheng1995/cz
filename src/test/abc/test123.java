aab2967921a199608be21d9d273d55c35ae3a399
chenzheng1995 <852110522@qq.com>
2021-04-19 09:10:17 +0800

    测试

---
+++ src/test/abc/test123.java
@@ -0,0 +1,11 @@
+﻿package test;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("这是测试代码");
+		System.out.println("类名首字母要大写");
+	}
+
+}
